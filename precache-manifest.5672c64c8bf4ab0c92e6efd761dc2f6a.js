f1d9b75bb672a19a57851589f41ea25d6dee75fe
Bboy <bornscience1@gmail.com>
2020-10-26 16:37:38 -0700

    Updates

---
+++ precache-manifest.5672c64c8bf4ab0c92e6efd761dc2f6a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f7be1bb87092b44c4b87cc8b3454467",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9108800d867a3eccc488",
+    "url": "/portfolio/static/css/2.bef3ccba.chunk.css"
+  },
+  {
+    "revision": "d9b31b5b14e57d12f426",
+    "url": "/portfolio/static/css/main.6a5fec2a.chunk.css"
+  },
+  {
+    "revision": "9108800d867a3eccc488",
+    "url": "/portfolio/static/js/2.33dd5852.chunk.js"
+  },
+  {
+    "revision": "65f8a716a843f6a37057e989c1b679a6",
+    "url": "/portfolio/static/js/2.33dd5852.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9b31b5b14e57d12f426",
+    "url": "/portfolio/static/js/main.c34bf953.chunk.js"
+  },
+  {
+    "revision": "e63ac8d85df9dec8b092",
+    "url": "/portfolio/static/js/runtime-main.b97a0a97.js"
+  },
+  {
+    "revision": "207b1eddcb2dc44109f4db4864fcd0af",
+    "url": "/portfolio/static/media/bboy.207b1edd.png"
+  },
+  {
+    "revision": "0bf7e2c86baaca3ff7f72958cff502b3",
+    "url": "/portfolio/static/media/next-arrow.0bf7e2c8.svg"
+  }
+]);
